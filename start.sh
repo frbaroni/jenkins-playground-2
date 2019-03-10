47fac003ea81800e8afbb1cdce99640c389cbed6
Fernando Baroni <fernando@baroni.tech>
2019-03-10 17:32:44 -0300

    Added Jenkinsfile sample

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run --name btech-jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home btech-jenkins
+docker rm btech-jenkins || true
+docker run --name btech-jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home -v /home/fernando/p/jenkins:/btech_jenkins btech-jenkins

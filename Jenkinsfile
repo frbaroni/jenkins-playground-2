527151a7964233170f4f944651dfd9224d62b03b
Fernando Baroni <fernando@baroni.tech>
2019-03-10 17:56:05 -0300

    Added basic version generation logic

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,15 @@
 def spec = [
+  project: 'My Project A',
   build_cmd: 'node --version',
   test_cmd: 'node --version',
 ]
+
+def version_file = new File("/build_data/$spec.project")
+env.BUILD_ID = version_file.text.toInteger() + 1
+version_file.text = env.BUILD_ID.toString()
+version_file.close()
+currentBuild.displayName = "#" + env.BUILD_ID
+
 
 pipeline {
     agent {
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
+docker stop btech-jenkins || true
 docker rm btech-jenkins || true
-docker run --name btech-jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home -v /home/fernando/p/jenkins:/btech_jenkins btech-jenkins
+docker run --name btech-jenkins -d -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home -v /home/fernando/p/jenkins/build_data:/build_data -v /home/fernando/p/jenkins:/btech_jenkins btech-jenkins
